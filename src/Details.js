b1dc93fdc22430766df424a4de2bbca82788cf5c
Manuel Spigolon <behemoth89@gmail.com>
2022-11-07 19:04:38 +0100

    step: error boundaries

--- src/Details.js
+++ src/Details.js
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 
 import Carousel from "./Carousel";
 
+import ErrorBoundary from "./ErrorBoundaries";
+
 // Old style: Class component
 class Details extends Component {
   constructor(props) {
@@ -30,6 +32,10 @@ class Details extends Component {
       return <h2>Loading...</h2>;
     }
 
+    if (this.props.params.id === '2') {
+      throw new Error("Error boundary test");
+    }
+
     const { animal, breed, city, state, description, name, images } =
       this.state;
 
@@ -54,6 +60,10 @@ class Details extends Component {
 // export default withRouter(Details);
 const WrappedDetails = () => {
   const params = useParams();
-  return <Details params={params} />;
+  return (
+    <ErrorBoundary>
+      <Details params={params} />
+    </ErrorBoundary>
+  );
 };
 export default WrappedDetails;
