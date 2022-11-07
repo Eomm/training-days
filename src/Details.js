5b1a8572df598fda135bba7739d57063ae77c10a
Manuel Spigolon <behemoth89@gmail.com>
2022-11-07 18:35:58 +0100

    step: old class components

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,59 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
 
-const Details = (props) => {
-  const { id } = useParams();
+import Carousel from "./Carousel";
 
-  return <h2>{id}</h2>;
+// Old style: Class component
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  // like useEffect(fun, [])
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    // it causes a re-render
+    this.setState({
+      loading: false,
+      ...json.pets[0],
+    });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+        {/* <div>
+          <img src={images[0]} alt={name} />
+        </div> */}
+      </div>
+    );
+  }
+}
+
+// withRouter explained:
+// export default withRouter(Details);
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
 };
-
-export default Details;
+export default WrappedDetails;
